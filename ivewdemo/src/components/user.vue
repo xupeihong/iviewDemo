d1c783ac30aa56bc3a72fee31289bc3388001a75
xupeihong <937603659@qq.com>
2018-08-28 23:46:25 +0800

    1

---
+++ ivewdemo/src/components/user.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="user">
+		<p>点击测试子组件</p>
+	</div>
+</template>
+<script >
+	export default{
+		name:'user',
+		data(){
+			return{}
+		}
+	}
+</script>
+<style scoped></style>
--- ivewdemo/src/router/index.js
+++ ivewdemo/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import index from '@/components/index'
+// import user from '@/components/user'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+    path:'/index',
+    name:'index',
+    component:index
     }
+    // {
+    //   path: '/user',
+    //   name: 'user',
+    //   component: user
+    // },
   ]
 })
